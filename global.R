b2f6e217a0865f5e6ba99e20000fcb04477ef469
frandoLin <41569547+frandoLin@users.noreply.github.com>
2020-01-16 14:42:53 -0600

    adding some features

--- global.R
+++ global.R
@@ -5,29 +5,72 @@ library(colourpicker)
 library(NLP)
 library(wordcloud)
 library(wordcloud2)
+library(ggplot2)   
+library(gganimate)
 library(tm)
-
-usPermVisas <-  read_csv("data/us_perm_visas.csv") 
+library(hrbrthemes)
+library(viridis)
 
 #some colors for plots
 fillColor = "#FFA07A"
 fillColor2 = "#F1C40F"
+
+usPermVisas <-  read.csv("data/us_perm_visas.csv", encoding = "UTF-8") 
+
+usPermVisas$state <- toupper(usPermVisas$state) 
+
+usPermVisas$employer_city <- usPermVisas$employer_city %>% 
+  iconv(to = "utf-8") %>%
+  toupper()
+
+#wage comparision between applicants and glassdoor
+glassdoor <- read_xlsx("data/glassdoor.xlsx")
+
+foreign_local_wage <- usPermVisas %>% 
+  mutate(job_title = case_when(
+    str_detect(job_info_job_title, "database engineer") ~ "database engineer",
+    str_detect(job_info_job_title, "database administrator") ~ "database administrator",
+    str_detect(job_info_job_title, "data analyst") ~ "data analyst",
+    str_detect(job_info_job_title, "warehousing") ~ "data warehousing engineer",
+    str_detect(job_info_job_title, "programer analyst") ~ "programmer analyst",
+    str_detect(job_info_job_title, "data scientist") ~ "data scientist",
+    str_detect(job_info_job_title, "quality assurance") ~ "quality assurance analyst",
+    str_detect(job_info_job_title, "component design") ~ "component design engineer",
+    str_detect(job_info_job_title, "software developer") ~ "software developer",
+    str_detect(job_info_job_title, "software engineer") ~ "software developer",
+    str_detect(job_info_job_title, "consultant") ~ "consultant",
+    str_detect(job_info_job_title, "test") ~ "test engineer",
+    str_detect(job_info_job_title, "project manager") ~ "project manager",
+    str_detect(job_info_job_title, "assistant professor") ~ "assistant professor",
+    str_detect(job_info_job_title, "cook") ~ "cook",
+  )) %>% 
+  drop_na(job_title) %>% 
+  group_by(job_title) %>% 
+  summarise(wage_mean = mean(wage, na.rm=TRUE)) %>% 
+  inner_join(glassdoor, by = 'job_title') %>% 
+  mutate(difference = wage_local - wage_mean)
 
 #columns for wordcloud generation
 job <- usPermVisas$job_info_job_title %>% 
   iconv(to = "utf-8") %>% 
   paste(collapse = ", ") #convert the column to string 
 
+education <- usPermVisas%>% 
+  filter(job_info_education != 'None') %>% 
+  count(job_info_education )
+
 stop_words <- c('senior', 'manager', 'director', 'and', 'ii', 'sr', 'and', 'business', 'lead', 'developer', 'of',
                 'v', 'worker', 'staff', 'services', 'iv','iii','technology','food','i','multiple', 'other', 'or', 
                 'commercial','rd','meat','office','group','serving','computer','development','computer','technical',
-                'it','poultry','associate') 
+                'it','poultry','associate', 'design','engineering','principal') 
 
 #fuction for wordcloud generation
 create_wordcloud <- function(data, num_words = 100, background) {
   
   # If text is provided, convert it to a dataframe of word frequencies
-  docs <- data_frame(text = data) %>% 
+  
+  if(data == job){
+    docs <- data_frame(text = data) %>% 
     mutate(text = tolower(text)) %>% 
     mutate(text = removeNumbers(text)) %>%
     mutate(text = str_remove_all(text, '[[:punct:]]')) %>% 
@@ -39,18 +82,106 @@ create_wordcloud <- function(data, num_words = 100, background) {
     filter(!tokens %in% stop_words)
   
   # Grab the top n most common words
-  docs <- head(docs, n = num_words)
-  if (nrow(docs) == 0) {
-    return(NULL)
-  }
-  
-  wordcloud2(docs, color = 'random-dark', backgroundColor = background)
+    docs <- head(docs, n = num_words)
+    if (nrow(docs) == 0) {
+      return(NULL)
+    }
+    
+    wordcloud2(docs, color = 'random-dark', backgroundColor = background, widgetsize = 250)
+  } else {
+    wordcloud2(data, color = 'random-dark', backgroundColor = background, widgetsize = 250)
+    }
 }
 
+#trump v.s. obama by counts, salary and process time
+obama_count <- usPermVisas %>% 
+  filter(decision_year %in% c(2014, 2015, 2016) & case_status == 'Certified') %>% 
+  count(state, name = "o_count") 
+
+trump_count <- usPermVisas %>% 
+  filter(decision_year %in% c(2017, 2018, 2019) & case_status == 'Certified') %>% 
+  count(state, name = "t_count")
+
+obama_trump_count <- obama_count %>%
+  full_join(trump_count, by = c("state"))  %>% 
+  filter(!is.na(state)) %>% 
+  replace_na(list(o_count = 0.1, t_count = 0.1)) %>% 
+  mutate(count_change = t_count - o_count)
+
+
+#weighted salary by state
+o_salary_weight <- usPermVisas %>% 
+  filter(decision_year %in% c(2014,2015,2016)&case_status=='Certified'& !is.na(state) & !is.na(employer_city)) %>% 
+  count(state, employer_city, name = "o_count_by_city") 
+
+o_salary <- usPermVisas %>% 
+  filter(case_status=='Certified'& !is.na(state) & !is.na(employer_city) & !is.na(wage)) %>% 
+  select(state, employer_city, wage) %>% 
+  inner_join(o_salary_weight, by = c("state", 'employer_city')) %>% 
+  group_by(state) %>% 
+  summarise(o_wage_mean = weighted.mean(wage, o_count_by_city))
+
+t_salary_weight <- usPermVisas %>% 
+  filter(decision_year %in% c(2017,2018,2019)&case_status=='Certified'& !is.na(state) & !is.na(employer_city)) %>% 
+  count(state, employer_city, name = "t_count_by_city")
+
+t_salary <- usPermVisas %>% 
+  filter(case_status=='Certified'& !is.na(state) & !is.na(employer_city) & !is.na(wage)) %>% 
+  select(state, employer_city, wage) %>% 
+  inner_join(t_salary_weight, by = c("state", 'employer_city')) %>% 
+  group_by(state) %>% 
+  summarise(t_wage_mean = weighted.mean(wage, t_count_by_city))
+
+o_t_wage <- o_salary %>% 
+  inner_join(t_salary, by='state') %>% 
+  mutate(change = t_wage_mean - o_wage_mean)
+
+
+#weighted process time by state
+o_time <- usPermVisas %>% 
+  filter(case_status=='Certified'& !is.na(state) & !is.na(employer_city) & !is.na(processing_time)) %>% 
+  select(state, employer_city, processing_time) %>% 
+  inner_join(o_salary_weight, by = c("state", 'employer_city')) %>% 
+  group_by(state) %>% 
+  summarise(o_time_mean = weighted.mean(processing_time, o_count_by_city))
+
+t_time <- usPermVisas %>% 
+  filter(case_status=='Certified'& !is.na(state) & !is.na(employer_city) & !is.na(processing_time)) %>% 
+  select(state, employer_city, processing_time) %>% 
+  inner_join(t_salary_weight, by = c("state", 'employer_city')) %>% 
+  group_by(state) %>% 
+  summarise(t_time_mean = weighted.mean(processing_time, t_count_by_city))
+
+o_t_time <- o_time %>% 
+  inner_join(t_time, by='state') %>% 
+  mutate(change = t_time_mean - o_time_mean)
+
+
 # Generate Options for dropdown menu in ui
-states <- usPermVisas %>% 
+state <- usPermVisas %>% 
   drop_na(state) %>% 
   pull(state) %>% 
-  toupper() %>% 
-  unique()
+  unique() %>% 
+  str_sort() %>% 
+  append("ALL", after = 2)
 
+word <- c('education', 'job')
+
+ggplot(o_t_time, aes(x=o_time_mean, xend=t_time_mean, y=state)) + 
+  geom_segment(aes(x=o_time_mean, 
+                   xend=t_time_mean, 
+                   y=state, 
+                   yend=state), 
+               color="#b2b2b2", size=1.5)+
+  geom_dumbbell(color="light blue", 
+                size_x=3.5, 
+                size_xend = 3.5,
+                colour_x=fillColor, 
+                colour_xend = fillColor2)+
+  labs(x= 'Year', y= 'State',
+       title="Processing Time Comparison by State", 
+       subtitle="Obama vs Trump")+
+  geom_text(color="black", size=2, hjust=-0.5,
+            aes(x=o_time_mean, label=t_time_mean))+
+  geom_text(aes(x=o_time_mean, label=t_time_mean), 
+            color="black", size=2, hjust=1.5)
--- work_visa_us.R
+++ work_visa_us.R
@@ -15,6 +15,10 @@ library(ggplot2)
 library(gganimate)
 library(gifski)
 library(png)
+library(chorddiag)
+library(hrbrthemes)
+library(viridis)
+library(ggalt)
 
 
 #function to normalize variables
@@ -106,6 +110,26 @@ write.csv(visa_all,"E:\\NSS\\nss_data_science\\work_visa_us\\data\\us_perm_visas
 
 usPermVisas =  read_csv("data/us_perm_visas.csv")
 
+
+#add approval rate by company and fill by the scale of the companys
+usPermVisas %>% 
+  filter(case_status == 'Certified') %>% 
+  group_by(state) %>%
+  summarise(CountOfEmployerName = n()) %>%
+  arrange(desc(CountOfEmployerName)) %>%
+  mutate(state = reorder(state, CountOfEmployerName)) %>%
+  head(20) %>% 
+  
+  ggplot(aes(x = state,y = CountOfEmployerName)) +
+  geom_bar(stat='identity',colour="white", fill =fillColor) +
+  geom_text(aes(x = state, y = 1, label = paste0("(",CountOfEmployerName,")",sep="")),
+            hjust=0, vjust=.5, size = 4, colour = 'black',
+            fontface = 'bold') +
+  labs(x = 'Employer Name', y = 'Count Of Visa Applications in NYC', title = 'Employers in NYC and Visas') +
+  coord_flip() + 
+  theme_bw()
+
+
 #Create a vector containing only the text
 job <- usPermVisas$job_info_job_title %>% 
   iconv(to = "utf-8") %>% 
@@ -116,6 +140,7 @@ stop_words <- c('senior', 'manager', 'director', 'and', 'ii', 'sr', 'and', 'busi
                 'commercial','rd','meat','office','group','serving','computer','development','computer','technical',
                 'it','poultry') 
 
+#change count to frequency
 docs <- data_frame(text = job) %>% 
   mutate(text = tolower(text)) %>% 
   mutate(text = removeNumbers(text)) %>%
@@ -127,27 +152,79 @@ docs <- data_frame(text = job) %>%
   filter(n>5000) %>% 
   filter(!tokens %in% stop_words)
 
-wordcloud2(docs, color = 'random-dark')
+wordcloud2(education, color = 'random-dark')
 
 fillColor = "#FFA07A"
 fillColor2 = "#F1C40F"
 
+
+#degree
 usPermVisas %>% 
-  filter(state == None & case_status == 'Certified') %>% 
-  group_by(employer_name) %>%
-  summarise(CountOfEmployerName = n()) %>%
-  arrange(desc(CountOfEmployerName)) %>%
-  mutate(employer_name = reorder(employer_name, CountOfEmployerName)) %>%
-  head(20) %>% 
-  
-  ggplot(aes(x = employer_name,y = CountOfEmployerName)) +
-  geom_bar(stat='identity',colour="white", fill =fillColor) +
-  geom_text(aes(x = employer_name, y = 1, label = paste0("(",CountOfEmployerName,")",sep="")),
-            hjust=0, vjust=.5, size = 4, colour = 'black',
-            fontface = 'bold') +
-  labs(x = 'Employer Name', y = 'Count Of Visa Applications in NYC', title = 'Employers in NYC and Visas') +
-  coord_flip() + 
-  theme_bw()
+  filter(job_info_education != 'None') %>% 
+  filter(case_status %in% c('Certified','Certified-Expired')) %>% 
+  count(case_received_year, job_info_education, name = "degree_count") %>% 
+  ggplot( aes(x=case_received_year, y=degree_count, group=job_info_education, color=job_info_education)) +
+  geom_line() +
+  geom_point() +
+  scale_color_hue('job_info_education') +
+  ggtitle("The Change in The Education of Applicants over the years") +
+  theme_ipsum() +
+  xlab("year") +
+  ylab("Number of babies born") +
+  transition_reveal(year)
+
+
+#job
+#tag1 hist
+breaks = seq(0,300000,40000)
+
+usPermVisas %>%
+  filter(case_status == 'Certified' ) %>%
+  ggplot(aes(wage)) +
+  scale_x_continuous(limits = c(0, 300000),breaks=breaks ) +
+  geom_histogram(binwidth = 10000,,fill = c("red")) +
+  labs(x = 'Amount in 9089', y = 'Count', 
+       title = 'Certified US Visa PW Amount 9089 distribution') +  theme_bw()
+
+#tag2 v.s. local salary
+glassdoor <- read_xlsx("data/glassdoor.xlsx")
+
+foreign_local_wage <- usPermVisas %>% 
+mutate(job_title = case_when(
+  str_detect(job_info_job_title, "database engineer") ~ "database engineer",
+  str_detect(job_info_job_title, "database administrator") ~ "database administrator",
+  str_detect(job_info_job_title, "data analyst") ~ "data analyst",
+  str_detect(job_info_job_title, "warehousing") ~ "data warehousing engineer",
+  str_detect(job_info_job_title, "programer analyst") ~ "programmer analyst",
+  str_detect(job_info_job_title, "data scientist") ~ "data scientist",
+  str_detect(job_info_job_title, "quality assurance") ~ "quality assurance analyst",
+  str_detect(job_info_job_title, "component design") ~ "component design engineer",
+  str_detect(job_info_job_title, "software developer") ~ "software developer",
+  str_detect(job_info_job_title, "software engineer") ~ "software developer",
+  str_detect(job_info_job_title, "consultant") ~ "consultant",
+  str_detect(job_info_job_title, "test") ~ "test engineer",
+  str_detect(job_info_job_title, "project manager") ~ "project manager",
+  str_detect(job_info_job_title, "assistant professor") ~ "assistant professor",
+  str_detect(job_info_job_title, "cook") ~ "cook",
+)) %>% 
+  drop_na(job_title) %>% 
+  group_by(job_title) %>% 
+  summarise(wage_mean = mean(wage, na.rm=TRUE)) %>% 
+  inner_join(glassdoor, by = 'job_title') %>% 
+  mutate(difference = wage_local - wage_mean)
+
+
+foreign_local_wage %>% 
+  ggplot(aes(x = reorder(job_title, difference), y = difference,
+             fill = difference > 0))+
+  geom_bar(stat = "identity")+
+  coord_flip()+
+  labs(x = "Country", y = "GDP per Capita Change %",
+       title = "Percentage chage in GDP per Capita",
+       subtitles = "Americas (1997 to 2007)")+
+  theme_minimal()+
+  guides(fill = FALSE)
+
 
 #the plot for the changes in the citizenship of the visa applicants over the years
 countCountry <- usPermVisas %>% 
@@ -196,3 +273,151 @@ anim <- ggplot(countCountry, aes(rank, group = nationality))+
 
 animate(anim, nframes = 350,fps = 25,  width = 1200, height = 1000, 
         renderer = gifski_renderer("gganim.gif"))
+
+#trump v.s. obama by counts, salary and process time
+obama_count <- usPermVisas %>% 
+  filter(decision_year %in% c(2014, 2015, 2016) & case_status == 'Certified') %>% 
+  count(state, name = "o_count") 
+
+trump_count <- usPermVisas %>% 
+  filter(decision_year %in% c(2017, 2018, 2019) & case_status == 'Certified') %>% 
+  count(state, name = "t_count")
+
+obama_trump_count <- obama_count %>%
+  full_join(trump_count, by = c("state"))  %>% 
+  filter(!is.na(state)) %>% 
+  replace_na(list(o_count = 0.1, t_count = 0.1)) %>% 
+  mutate(count_change = t_count - o_count)
+
+#counts
+ggplot(obama_trump_count, aes(x=o_count, xend=t_count, y=state)) + 
+  #create a thick line between x and xend instead of using defaut 
+  #provided by geom_dubbell
+  geom_segment(aes(x=o_count, 
+                   xend=t_count, 
+                   y=state, 
+                   yend=state), 
+               color="#b2b2b2", size=1.5)+
+  geom_dumbbell(color="light blue", 
+                size_x=3.5, 
+                size_xend = 3.5,
+                #Note: there is no US:'color' for UK:'colour' 
+                # in geom_dumbbel unlike standard geoms in ggplot()
+                colour_x=fillColor, 
+                colour_xend = fillColor2)+
+  labs(x=NULL, y=NULL, 
+       title="Dumbbell Chart", 
+       subtitle="Obama vs Trump")+
+  geom_text(color="black", size=2, hjust=-0.5,
+            aes(x=o_count, label=o_count))+
+  geom_text(aes(x=t_count, label=t_count), 
+            color="black", size=2, hjust=1.5)
+
+#weighted salary by state
+o_salary_weight <- usPermVisas %>% 
+  filter(decision_year %in% c(2014,2015,2016)&case_status=='Certified'& !is.na(state) & !is.na(employer_city)) %>% 
+  count(state, employer_city, name = "o_count_by_city") 
+
+o_salary <- usPermVisas %>% 
+  filter(case_status=='Certified'& !is.na(state) & !is.na(employer_city) & !is.na(wage)) %>% 
+  select(state, employer_city, wage) %>% 
+  inner_join(o_salary_weight, by = c("state", 'employer_city')) %>% 
+  group_by(state) %>% 
+  summarise(o_wage_mean = weighted.mean(wage, o_count_by_city))
+
+t_salary_weight <- usPermVisas %>% 
+  filter(decision_year %in% c(2017,2018,2019)&case_status=='Certified'& !is.na(state) & !is.na(employer_city)) %>% 
+  count(state, employer_city, name = "t_count_by_city")
+
+t_salary <- usPermVisas %>% 
+  filter(case_status=='Certified'& !is.na(state) & !is.na(employer_city) & !is.na(wage)) %>% 
+  select(state, employer_city, wage) %>% 
+  inner_join(t_salary_weight, by = c("state", 'employer_city')) %>% 
+  group_by(state) %>% 
+  summarise(t_wage_mean = weighted.mean(wage, t_count_by_city))
+
+o_t_wage <- o_salary %>% 
+  inner_join(t_salary, by='state') %>% 
+  mutate(change = t_wage_mean - o_wage_mean)
+
+ggplot(o_t_wage, aes(x=o_wage_mean, xend=t_wage_mean, y=state)) + 
+  geom_segment(aes(x=o_wage_mean, 
+                   xend=t_wage_mean, 
+                   y=state, 
+                   yend=state), 
+               color="#b2b2b2", size=1.5)+
+  geom_dumbbell(color="light blue", 
+                size_x=3.5, 
+                size_xend = 3.5,
+                colour_x=fillColor, 
+                colour_xend = fillColor2)+
+  labs(x=NULL, y=NULL, 
+       title="Dumbbell Chart", 
+       subtitle="Obama vs Trump")+
+  geom_text(color="black", size=2, hjust=-0.5,
+            aes(x=o_wage_mean, label=t_wage_mean))+
+  geom_text(aes(x=o_wage_mean, label=t_wage_mean), 
+            color="black", size=2, hjust=1.5)
+
+#weighted process time by state
+o_time <- usPermVisas %>% 
+  filter(case_status=='Certified'& !is.na(state) & !is.na(employer_city) & !is.na(processing_time)) %>% 
+  select(state, employer_city, processing_time) %>% 
+  inner_join(o_salary_weight, by = c("state", 'employer_city')) %>% 
+  group_by(state) %>% 
+  summarise(o_time_mean = weighted.mean(processing_time, o_count_by_city))
+
+t_time <- usPermVisas %>% 
+  filter(case_status=='Certified'& !is.na(state) & !is.na(employer_city) & !is.na(processing_time)) %>% 
+  select(state, employer_city, processing_time) %>% 
+  inner_join(t_salary_weight, by = c("state", 'employer_city')) %>% 
+  group_by(state) %>% 
+  summarise(t_time_mean = weighted.mean(processing_time, t_count_by_city))
+
+o_t_time <- o_time %>% 
+  inner_join(t_time, by='state') %>% 
+  mutate(change = t_time_mean - o_time_mean)
+
+ggplot(o_t_time, aes(x=o_time_mean, xend=t_time_mean, y=state)) + 
+  geom_segment(aes(x=o_time_mean, 
+                   xend=t_time_mean, 
+                   y=state, 
+                   yend=state), 
+               color="#b2b2b2", size=1.5)+
+  geom_dumbbell(color="light blue", 
+                size_x=3.5, 
+                size_xend = 3.5,
+                colour_x=fillColor, 
+                colour_xend = fillColor2)+
+  labs(x=NULL, y=NULL, 
+       title="Dumbbell Chart", 
+       subtitle="Obama vs Trump")+
+  geom_text(color="black", size=2, hjust=-0.5,
+            aes(x=o_time_mean, label=t_time_mean))+
+  geom_text(aes(x=o_time_mean, label=t_time_mean), 
+            color="black", size=2, hjust=1.5)
+
+
+  
+
+
+
+students = data.frame(Math = c(50, 25, 5, 12),
+                      Art = c(10, 55, 5, 20),
+                      Science = c(45,12,29, 20),
+                      PE = c(24,67,27,15))
+
+students = as.matrix(students)
+row.names(students) = c("Section A", "Section B", "Section C", "Section D")
+
+chorddiag(students, type = "bipartite", groupnameFontsize = 14, groupnamePadding = 10, margin = 90)
+
+
+
+
+
+
+
+
+
+
